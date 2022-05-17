1afb373b31d0b70735c1400cbcc48507d5261607
Jonas Ethan Springer <springerjonasethan@gmail.com>
2022-05-17 13:39:17 +0200

    Started Adding new Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
+# the Model of my Task instances
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'tasks#index'
+  get '/add', to: 'tasks#create'
 end
